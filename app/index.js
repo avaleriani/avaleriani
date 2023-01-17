d78965cf4c2cfb53834459d4461b43e994362f34
Agu Valeriani <agvaleriani@gmail.com>
2023-01-17 23:09:48 +0100

    -updated command colors.

--- app/index.js
+++ app/index.js
@@ -2,9 +2,11 @@ const { render } = require('prettyjson');
 const data = require('./data')
 
 const renderOpts = {
-  dashColor: 'yellow',
-  keysColor: 'blue',
-  stringColor: 'red'
+  keysColor: 'italic',
+  stringColor: 'blue',
+  multilineStringColor: 'rainbow',
+  dashColor: 'black',
+  defaultIndentation: 4,
 }
 
 module.exports = ({ json }) => json ? JSON.stringify(data) : render(data, renderOpts)
